4ed8fc2ddc650ddfd438f697efdcf857a1965436
arche <zxk44944@qq.com>
2023-08-11 10:21:37 +0800

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,5 @@
-import { Suspense, lazy } from 'react';
-import { sleep } from './lib/js-utils';
-
-// the 'Hello' async component
-async function getHello() {
-  await sleep(5);
-  const Component = () => {
-    return <div>the hello async component</div>;
-  };
-  return {
-    default: Component,
-  };
-}
-
-const Hello = lazy(getHello);
-
-// the fallback component
-function Fallback() {
-  return <div>loading...</div>;
-}
+import Basic from './hello/basic';
 
 export default function App() {
-  return (
-    <Suspense fallback={<Fallback />}>
-      <p>
-        I am just in the `suspend`, you can not see me util the `hello` done
-        loaded
-      </p>
-      <Hello />
-    </Suspense>
-  );
+  return <Basic />;
 }
